637cdcda2332cfb6be4f4aba3fca224bc7052df5
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-24 12:26:07 -0500

    finish mobile view for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Navbar from "@/components/Navbar/Navbar";
 import "./home.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="home">
       <Navbar />
+      <section>
+        <h2>SO, YOU WANT TO TRAVEL TO</h2>
+        <h1>SPACE</h1>
+        <p>
+          {`Let's face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we'll give you a truly out of this world
+          experience!`}
+        </p>
+        <div className="circleContainer">
+          <div className="circle">
+            <Link className="link" href={"/Destination"}>
+              EXPLORE
+            </Link>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
