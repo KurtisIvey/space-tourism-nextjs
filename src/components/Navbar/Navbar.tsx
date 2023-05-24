165c9d63ecd16f5e95406049f7314ffb71bdd6ea
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-24 11:02:29 -0500

    finish mobile navbar base styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,15 +13,37 @@ const Navbar = (props: Props) => {
   // state for managing navbar menu on smaller res to trigger ternary for img to open or close
   const [navbarOpen, setNavbarOpen] = useState<boolean>(false);
 
+  const toggleNavbar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
   return (
     <nav className="navbar">
       <Image className="logo" src={logo} alt="logo" />
 
       {/* This will have a higher z index to sit on top of the slide out menu div that is positioned absolutely */}
       <Image
+        className="openCloseIcon"
         src={navbarOpen ? navMenuCloseIcon : hamburgerIcon}
         alt="hamburger menu icon"
+        onClick={toggleNavbar}
       />
+      <div className={`slidingMenu ${navbarOpen && "navbarOpen"}`}>
+        <ul>
+          <li>
+            <div>00</div>
+            <div>HOME</div>
+          </li>
+          <li>
+            <div>01</div> <div>DESTINATION</div>
+          </li>
+          <li>
+            <div>02</div> <div>CREW</div>
+          </li>
+          <li>
+            <div>03</div> <div>TECHNOLOGY</div>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
