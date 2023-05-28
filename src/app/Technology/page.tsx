05e44cd76475f81195561196df36329eacf99ec6
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-28 17:26:00 -0500

    update page.tsx and add technology.data.ts

--- src/app/Technology/page.tsx
+++ src/app/Technology/page.tsx
@@ -1,9 +1,18 @@
+"use client";
+
 import React from "react";
+import technologyData from "./technology.data";
+import Navbar from "@/components/Navbar/Navbar";
 
 type Props = {};
 
 const Technology = (props: Props) => {
-  return <div>Technology</div>;
+  return (
+    <main className="technology">
+      <Navbar activePage="technology" />
+      <section></section>
+    </main>
+  );
 };
 
 export default Technology;
