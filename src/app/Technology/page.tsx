cb79c19f7c2328719ac91f88b06bf53ad99092ce
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-28 17:39:31 -0500

    add windowWidth detectino to handle landscape img

--- src/app/Technology/page.tsx
+++ src/app/Technology/page.tsx
@@ -1,16 +1,43 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Technology.css";
 import technologyData from "./technology.data";
 import Navbar from "@/components/Navbar/Navbar";
+import Image from "next/image";
 
 type Props = {};
 
 const Technology = (props: Props) => {
+  const [index, setIndex] = useState<number>(0);
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    // Add event listener to track window resize
+    window.addEventListener("resize", handleResize);
+
+    // Initial window width
+    setWindowWidth(window.innerWidth);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <main className="technology">
       <Navbar activePage="technology" />
-      <section></section>
+      <section>
+        <h1>
+          <span>01</span> SPACE LAUNCH 101
+        </h1>
+        <Image src={technologyData[index]} />
+      </section>
     </main>
   );
 };
