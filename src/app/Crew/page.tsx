32c21d4be7fd3436245725d3d7d64930cac376ad
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-28 00:01:52 -0500

    add bg and h1 to crew.tsx

--- src/app/Crew/page.tsx
+++ src/app/Crew/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+import Navbar from "@/components/Navbar/Navbar";
 import React from "react";
+import "./Crew.css";
+import crewData from "./crew.data";
 
 type Props = {};
 
 const Crew = (props: Props) => {
-  return <div>Crew</div>;
+  return (
+    <main className="crew">
+      <Navbar activePage="crew" />
+      <h1>
+        <span>02</span> MEET YOUR CREW
+      </h1>
+    </main>
+  );
 };
 
 export default Crew;
