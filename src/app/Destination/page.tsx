77f03e49e9f55346a768ef67b4c11801bd6b1e07
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-28 00:47:43 -0500

    create file for checkwebpsupport function due to reuse

--- src/app/Destination/page.tsx
+++ src/app/Destination/page.tsx
@@ -4,36 +4,15 @@ import Navbar from "@/components/Navbar/Navbar";
 import React, { useState } from "react";
 import "./Destination.css";
 import Image from "next/image";
-import moonImage from "../../assets/destination/image-moon.png";
-import destinationInfo from "./destination.data";
-
+import destinationData from "./destination.data";
+import checkWebpSupport from "@/function/checkWebpSupport";
 type Props = {};
 
 const Destination = (props: Props) => {
   // sets index to load destination info data as it's an array
   const [dataIndex, setDataIndex] = useState(0);
-
   const supportsWebp = checkWebpSupport();
 
-  function checkWebpSupport() {
-    const elem = document.createElement("canvas");
-    if (elem.getContext && elem.getContext("2d")) {
-      return elem.toDataURL("image/webp").indexOf("data:image/webp") === 0;
-    }
-    return false;
-  }
-
-  const dummyData = {
-    name: "Moon",
-    images: {
-      png: "../../assets/destination/image-moon.png",
-      webp: "../../assets/destination/image-moon.webp",
-    },
-    description:
-      "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-    distance: "384,400 km",
-    travel: "3 days",
-  };
   return (
     <main className="destination">
       <Navbar activePage="destination" />
@@ -44,16 +23,16 @@ const Destination = (props: Props) => {
         {supportsWebp ? (
           <Image
             className="planetImage"
-            src={destinationInfo[dataIndex].images.webp}
-            alt={destinationInfo[dataIndex].name}
+            src={destinationData[dataIndex].images.webp}
+            alt={destinationData[dataIndex].name}
             width={500}
             height={300}
           />
         ) : (
           <Image
             className="planetImage"
-            src={destinationInfo[dataIndex].images.png}
-            alt={destinationInfo[dataIndex].name}
+            src={destinationData[dataIndex].images.png}
+            alt={destinationData[dataIndex].name}
             width={500}
             height={300}
           />
@@ -85,16 +64,16 @@ const Destination = (props: Props) => {
               TITAN
             </li>
           </ul>
-          <h2>{destinationInfo[dataIndex].name}</h2>
-          <p>{destinationInfo[dataIndex].description}</p>
+          <h2>{destinationData[dataIndex].name}</h2>
+          <p>{destinationData[dataIndex].description}</p>
           <div className="metricsBigContainer">
             <div className="metricContainer">
               <h3>AVG. DISTANCE</h3>
-              <div>{destinationInfo[dataIndex].distance}</div>
+              <div>{destinationData[dataIndex].distance}</div>
             </div>
             <div className="metricContainer">
               <h3>EST. TRAVEL TIME</h3>
-              <div>{destinationInfo[dataIndex].travel}</div>
+              <div>{destinationData[dataIndex].travel}</div>
             </div>
           </div>
         </div>
