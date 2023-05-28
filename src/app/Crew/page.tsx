a9145c2ff1c22c17b427bbe960e7cd09f6415a5f
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-28 00:14:15 -0500

    update h1 styling, add crew member container styling

--- src/app/Crew/page.tsx
+++ src/app/Crew/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/components/Navbar/Navbar";
 import React from "react";
 import "./Crew.css";
 import crewData from "./crew.data";
+import Image from "next/image";
 
 type Props = {};
 
@@ -12,9 +13,30 @@ const Crew = (props: Props) => {
   return (
     <main className="crew">
       <Navbar activePage="crew" />
-      <h1>
-        <span>02</span> MEET YOUR CREW
-      </h1>
+      <section>
+        <h1>
+          <span>02</span> MEET YOUR CREW
+        </h1>
+        <div className="imageContainer">
+          <Image
+            src={dummyData.images.png}
+            alt={dummyData.name}
+            className="crewPhoto"
+          />
+        </div>
+        <div className="detailsContainer">
+          <ul>
+            {/* nav dots */}
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <h3>{dummyData.role}</h3>
+          <h2>{dummyData.name}</h2>
+          <p>{dummyData.bio}</p>
+        </div>
+      </section>
     </main>
   );
 };
