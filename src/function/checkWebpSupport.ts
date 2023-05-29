af8b624330efe40710ce9b79749b89eaafb2a686
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-05-29 12:12:23 -0500

    update checkwebpsupport.ts to work with next.js

--- src/function/checkWebpSupport.ts
+++ src/function/checkWebpSupport.ts
@@ -1,7 +1,14 @@
 export default function checkWebpSupport() {
+  if (typeof document === "undefined") {
+    // WebP support cannot be determined on the server-side rendering
+    return false;
+  }
+
   const elem = document.createElement("canvas");
   if (elem.getContext && elem.getContext("2d")) {
-    return elem.toDataURL("image/webp").indexOf("data:image/webp") === 0;
+    const webpData = elem.toDataURL("image/webp");
+    return webpData.indexOf("data:image/webp") === 0;
   }
+
   return false;
 }
